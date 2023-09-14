7a6ca8184f231f5d1b15ea200f4c425e8734f955
Pierpaolo Colasante <colasantepierpaolo@gmail.com>
2023-09-14 02:46:40 +0200

    first commit

---
+++ js/todolist.js
@@ -0,0 +1,32 @@
+const newlist = document.getElementById('to-do-list')
+newlist.addEventListener('submit', function (e) {
+  e.preventDefault()
+  const createNewList = document.getElementById('list')
+
+  const newOggList = {
+    Lista1: createNewList.value,
+  }
+
+  console.log(newOggList)
+
+  const nuovaAreaLista = document.createElement('div')
+  nuovaAreaLista.classList.add('add-class')
+  nuovaAreaLista.innerHTML = `${newOggList.Lista1} <button onclick="deleteList(event)">ELIMINA</button> <button onclick="spuntList(event)">SPUNTA</button>`
+
+  const applicoLista = document.getElementById('salva-lista')
+  applicoLista.appendChild(nuovaAreaLista)
+  createNewList.value = ''
+})
+
+const deleteList = function (e) {
+  const clickElimina = e.target
+  const alertRemove = confirm('Sicuro di eliminare?')
+  if (alertRemove) {
+    clickElimina.parentElement.remove()
+  }
+}
+const spuntList = function (e) {
+  const spunta = e.target
+  console.log('Spuntato')
+  spunta.parentElement.style.textDecoration = 'line-through'
+}
